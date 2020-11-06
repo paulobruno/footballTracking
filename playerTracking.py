3550f15962df627e080896ce47bc4fe41e7c8ccc
Paulo Bruno <pbrunosousa@gmail.com>
2020-11-05 21:13:55 -0300

    Detect players using HOG in video

--- playerTracking.py
+++ playerTracking.py
@@ -1,15 +1,29 @@
 import cv2
+from time import sleep
 
-video = cv2.VideoCapture('demo-s.mp4')
+
+video = cv2.VideoCapture('videos/temp_gol_1.mp4')
 
 ret, frame = video.read()
 
+hog = cv2.HOGDescriptor() 
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector()) 
+
 while frame is not None:
 
-    cv2.imshow('teste', frame)    
-    cv2.waitKey(33)
-    
+    frame = cv2.resize(frame, (960, 540))
+
+    players, _ = hog.detectMultiScale(frame, winStride=(4, 4), padding=(0, 0), scale=1.05)
+
+    for i in range(len(players)):
+        p_x, p_y, p_w, p_h = players[i]
+        cv2.rectangle(frame, (p_x, p_y), (p_x + p_w, p_y + p_h), (0, 255, 0), 3)
+
+    cv2.imshow('teste', frame)
+    cv2.waitKey(1)
+
     ret, frame = video.read()
+
 
 video.release()
 cv2.destroyAllWindows()
